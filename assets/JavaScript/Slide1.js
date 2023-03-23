931d12696a66376dcbab67cb9def6ad62df6b957
pradeep3091 <61347523+pradeep3091@users.noreply.github.com>
2023-03-23 20:14:28 +0530

    Add files via upload

---
+++ assets/JavaScript/Slide1.js
@@ -0,0 +1,27 @@
+﻿let temp = 0;
+
+function controller(x) {
+    temp = temp + x;
+    slideShow(temp);
+}
+
+slideShow(temp);
+
+
+function slideShow(num) {
+
+    let totalslides = document.querySelectorAll(".Slidecontect");
+    if (num == totalslides.length) {
+        num = 0;
+        temp = 0;
+    }
+    if (num < 0) {
+        num - totalslides.length - 1;
+        temp = totalslides.length - 1;
+    }
+
+    for (let i = 0; i < totalslides.length; i++) {
+        totalslides[i].style.display = "none";
+    }
+    totalslides[temp].style.display = "block";
+}
